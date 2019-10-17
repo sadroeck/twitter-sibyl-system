1afc9cbc501b21e81fbbcbc3996d2c8e86942957
Sam De Roeck <sam.de.roeck@wdc.com>
2019-10-17 12:42:46 +0200

    [refac] Always restart streams after hitting rate limit or other errors. Use tokio_timer::Delay as a future to exhaust before continuing

--- src/scraper/rate_controlled_stream.rs
+++ src/scraper/rate_controlled_stream.rs
@@ -1,7 +1,8 @@
 use futures::future::Future;
 use futures::{Async, FlattenStream, Stream};
-use log::info;
+use log::{error, info};
 use std::time::{Duration, Instant};
+use tokio_timer::Delay;
 use twitter_stream::error::Error as TwitterError;
 use twitter_stream::{
     types::StatusCode, FutureTwitterStream, Token, TwitterStream, TwitterStreamBuilder,
@@ -32,17 +33,15 @@ impl RateLimitedStream {
             inner: create_stream(api_token.clone(), topic.as_str()),
             topic,
             api_token,
-            halted_since: None,
             state: StreamAction::Continue,
         }
     }
 }
 
+#[derive(Debug)]
 enum StreamAction {
     Continue,
-    RestartNow,
-    RestartAfter(Duration),
-    Halt(Duration),
+    RestartAfter(Delay),
     Exit,
 }
 
@@ -50,9 +49,15 @@ enum StreamAction {
 /// c.f.r. https://developer.twitter.com/en/docs/basics/response-codes
 fn process_twitter_error(status_code: StatusCode) -> StreamAction {
     match status_code.as_u16() {
-        410 => StreamAction::RestartNow,
-        420 | 429 => StreamAction::Halt(Duration::from_secs(20)),
-        304 => StreamAction::RestartAfter(Duration::from_secs(300)),
+        410 => StreamAction::RestartAfter(Delay::new(Instant::now())),
+        420 | 429 => StreamAction::RestartAfter(Delay::new(
+            Instant::now().checked_add(Duration::from_secs(60)).unwrap(),
+        )),
+        304 => StreamAction::RestartAfter(Delay::new(
+            Instant::now()
+                .checked_add(Duration::from_secs(300))
+                .unwrap(),
+        )),
         400 | 401 | 403 | 404 | 406 | 422 => StreamAction::Exit,
         _ => StreamAction::Continue,
     }
@@ -63,60 +68,53 @@ impl Stream for RateLimitedStream {
     type Error = <TwitterStream as Stream>::Error;
 
     fn poll(&mut self) -> Result<Async<Option<Self::Item>>, Self::Error> {
-        match &self.state {
+        match &mut self.state {
             StreamAction::Continue => match self.inner.poll() {
                 Err(TwitterError::Http(status_code)) => {
                     self.state = process_twitter_error(status_code);
+                    match &self.state {
+                        StreamAction::RestartAfter(delay) => error!(
+                            "[{topic}] Reached twitter API limit, restarting stream after delay: {delay:?}",
+                            topic = &self.topic,
+                            delay = delay.deadline()
+                        ),
+                        StreamAction::Continue => (),
+                        StreamAction::Exit => (),
+                    }
+                    futures::task::current().notify();
                     Ok(Async::NotReady)
                 }
-                Err(other_err) => Err(other_err),
+                Err(other_err) => {
+                    error!("Received API error {}", other_err);
+                    Err(other_err)
+                }
                 Ok(Async::NotReady) => Ok(Async::NotReady),
                 Ok(Async::Ready(Some(content))) => Ok(Async::Ready(Some(content))),
                 Ok(Async::Ready(None)) => {
-                    self.state = StreamAction::Halt(Duration::from_secs(2));
-                    Ok(Async::NotReady)
+                    error!("[{topic}] Stream has stoppped", topic = &self.topic);
+                    Ok(Async::Ready(None))
                 }
             },
-            StreamAction::RestartNow => {
-                info!("Restarting topic stream immediately");
-                self.inner = create_stream(self.api_token.clone(), self.topic.as_str());
-                Ok(Async::NotReady)
-            }
-            StreamAction::RestartAfter(wait_time) => {
-                let now = Instant::now();
-                match self.halted_since.map(|instant| now.duration_since(instant)) {
-                    None => {
-                        info!("Restarting topic stream after {:?}", &wait_time);
-                        self.halted_since = Some(now);
-                        Ok(Async::NotReady)
-                    }
-                    Some(elapsed) => {
-                        if elapsed > *wait_time {
-                            self.inner = create_stream(self.api_token.clone(), self.topic.as_str());
-                            self.state = StreamAction::Continue;
-                        }
-                        Ok(Async::NotReady)
-                    }
+            StreamAction::RestartAfter(ref mut delay) => match delay.poll() {
+                Ok(Async::Ready(())) => {
+                    futures::task::current().notify();
+                    info!("[{topic}] Restart stream", topic = &self.topic);
+                    self.state = StreamAction::Continue;
+                    self.inner = create_stream(self.api_token.clone(), self.topic.as_str());
+                    Ok(Async::NotReady)
                 }
-            }
-            StreamAction::Halt(wait_time) => {
-                let now = Instant::now();
-                match self.halted_since.map(|instant| now.duration_since(instant)) {
-                    None => {
-                        info!("Halting topic stream for {:?}", &wait_time);
-                        self.halted_since = Some(now);
-                        Ok(Async::NotReady)
-                    }
-                    Some(elapsed) => {
-                        if elapsed > *wait_time {
-                            self.state = StreamAction::Continue;
-                        }
-                        Ok(Async::NotReady)
-                    }
+                Ok(Async::NotReady) => Ok(Async::NotReady),
+                Err(err) => {
+                    error!(
+                        "[{topic}] Could not wait for stream restart: {err}",
+                        topic = &self.topic,
+                        err = err
+                    );
+                    Err(TwitterError::TimedOut)
                 }
-            }
+            },
             StreamAction::Exit => {
-                panic!("Cancel twitter topic stream");
+                panic!("[{topic}] Cancel twitter topic stream", topic = &self.topic);
             }
         }
     }

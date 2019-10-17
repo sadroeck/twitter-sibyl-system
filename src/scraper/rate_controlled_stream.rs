e285ee098e3b9e813e3d4c68ea02215a594f03e2
Sam De Roeck <sam.de.roeck@wdc.com>
2019-10-17 15:24:04 +0200

    [impl] Apply exponential back-off when hitting twitter rate limits

--- src/scraper/rate_controlled_stream.rs
+++ src/scraper/rate_controlled_stream.rs
@@ -15,6 +15,7 @@ pub struct RateLimitedStream {
     topic: String,
     api_token: ApiToken,
     state: StreamAction,
+    previous_delay: u64,
 }
 
 fn create_stream(api_token: ApiToken, topic: &str) -> FlattenStream<FutureTwitterStream> {
@@ -33,6 +34,7 @@ impl RateLimitedStream {
             topic,
             api_token,
             state: StreamAction::Continue,
+            previous_delay: 0,
         }
     }
 }
@@ -40,23 +42,34 @@ impl RateLimitedStream {
 #[derive(Debug)]
 enum StreamAction {
     Continue,
-    RestartAfter(Delay),
+    RestartAfter { seconds: u64, waiter: Delay },
     Exit,
 }
 
 /// Derive an action based on twitter error codes
 /// c.f.r. https://developer.twitter.com/en/docs/basics/response-codes
-fn process_twitter_error(status_code: StatusCode) -> StreamAction {
+fn process_twitter_error(status_code: StatusCode, previous_delay: u64) -> StreamAction {
     match status_code.as_u16() {
-        410 => StreamAction::RestartAfter(Delay::new(Instant::now())),
-        420 | 429 => StreamAction::RestartAfter(Delay::new(
-            Instant::now().checked_add(Duration::from_secs(60)).unwrap(),
-        )),
-        304 => StreamAction::RestartAfter(Delay::new(
-            Instant::now()
-                .checked_add(Duration::from_secs(300))
-                .unwrap(),
-        )),
+        410 => StreamAction::RestartAfter {
+            seconds: 0,
+            waiter: Delay::new(Instant::now()),
+        },
+        420 | 429 => StreamAction::RestartAfter {
+            seconds: previous_delay + 60,
+            waiter: Delay::new(
+                Instant::now()
+                    .checked_add(Duration::from_secs(previous_delay + 60))
+                    .unwrap(),
+            ),
+        },
+        304 => StreamAction::RestartAfter {
+            seconds: 300,
+            waiter: Delay::new(
+                Instant::now()
+                    .checked_add(Duration::from_secs(300))
+                    .unwrap(),
+            ),
+        },
         400 | 401 | 403 | 404 | 406 | 422 => StreamAction::Exit,
         _ => StreamAction::Continue,
     }
@@ -70,13 +83,21 @@ impl Stream for RateLimitedStream {
         match &mut self.state {
             StreamAction::Continue => match self.inner.poll() {
                 Err(TwitterError::Http(status_code)) => {
-                    self.state = process_twitter_error(status_code);
+                    error!(
+                        "[{topic} Received error code {code}",
+                        topic = &self.topic,
+                        code = status_code
+                    );
+                    self.state = process_twitter_error(status_code, self.previous_delay);
                     match &self.state {
-                        StreamAction::RestartAfter(delay) => error!(
-                            "[{topic}] Reached twitter API limit, restarting stream after delay: {delay:?}",
-                            topic = &self.topic,
-                            delay = delay.deadline()
-                        ),
+                        StreamAction::RestartAfter { seconds, .. } => {
+                            error!(
+                                "[{topic}] Reached twitter API limit, restarting stream after {delay}s delay",
+                                topic = &self.topic,
+                                delay = seconds
+                            );
+                            self.previous_delay = *seconds;
+                        }
                         StreamAction::Continue => (),
                         StreamAction::Exit => (),
                     }
@@ -88,18 +109,21 @@ impl Stream for RateLimitedStream {
                     Err(other_err)
                 }
                 Ok(Async::NotReady) => Ok(Async::NotReady),
-                Ok(Async::Ready(Some(content))) => Ok(Async::Ready(Some(content))),
+                Ok(Async::Ready(Some(content))) => {
+                    self.previous_delay = 0;
+                    Ok(Async::Ready(Some(content)))
+                }
                 Ok(Async::Ready(None)) => {
                     error!("[{topic}] Stream has stoppped", topic = &self.topic);
                     Ok(Async::Ready(None))
                 }
             },
-            StreamAction::RestartAfter(ref mut delay) => match delay.poll() {
+            StreamAction::RestartAfter { ref mut waiter, .. } => match waiter.poll() {
                 Ok(Async::Ready(())) => {
-                    futures::task::current().notify();
                     info!("[{topic}] Restart stream", topic = &self.topic);
                     self.state = StreamAction::Continue;
                     self.inner = create_stream(self.api_token.clone(), self.topic.as_str());
+                    futures::task::current().notify();
                     Ok(Async::NotReady)
                 }
                 Ok(Async::NotReady) => Ok(Async::NotReady),
